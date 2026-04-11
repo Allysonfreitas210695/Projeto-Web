c39b7cbe1d6fb14f9a9214e1ba287df7b88e431d
Allysonfreitas210695 <alisonfr83@gmail.com>
2026-04-11 16:30:53 -0300

    fix: add ToastProvider to app root

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './app/providers/ThemeProvider';
+import { ToastProvider } from './components/ui/Toast';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="projeto-ia-theme">
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
